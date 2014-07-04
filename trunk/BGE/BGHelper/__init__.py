32b1ab88714fe9717383e84f99897b730a961172
solarlune@gmail.com <solarlune@gmail.com@5a5a8692-1f56-b8f9-d35c-1f04450fc82b>
2014-07-04 21:43:36 +0000

    Complete refactoring of BGE external BGE modules to highlight ease of use and maintainability. Some existing modules have been transformed into the new BGHelper unified versions:
    BGEAPIPlus.py > BGHelper/API.py
    RLG.py > BGHelper/RLG.py
    SFL.py > BGHelper/Filters.py
    BGInput.py / JoyProfile.py > BGHelper/Input.py
    
    While others like the RLG.py and LightTrail.py modules have been simply migrated, other modules like the TPWindow.py or BGUIHelper modules may be left behind.
    
    A central example blend file has been introduced to provide a central location from which one can examine the different elements of the BGHelper module. Note that this may be split out into several examples if necessary. 
    
    The BGInput and JoyProfile modules have been unified to have a central place to handle input and joystick profiles from. Along with some organizational changes, sprites have become classes with a few solid helper functions and variables to allow for more advanced functionality; see BGHelper/Example.blend for a working example of what Sprites can now do.

---
+++ trunk/BGE/BGHelper/__init__.py
@@ -0,0 +1,10 @@
+
+from . import API
+from . import Filters
+from . import Input
+from . import LightHelper
+from . import LightTrail
+from . import Math
+from . import Mesh
+from . import RLG
+from . import Sprites
