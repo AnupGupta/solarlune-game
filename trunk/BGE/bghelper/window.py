b7bb04844de6066063e39b679bcf6448ce330176
solarlune@gmail.com <solarlune@gmail.com@5a5a8692-1f56-b8f9-d35c-1f04450fc82b>
2014-08-17 22:49:19 +0000

    Adding of BGHelper module back with new name (bghelper, to align with PEP8) and renamed Python modules there-in.

---
+++ trunk/BGE/bghelper/window.py
@@ -0,0 +1,80 @@
+"""
+Copyright (c) 2013-2014 SolarLune
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+P.S. It would be nice if you could attribute me for the creation of this and my other scripts. Thanks!
+"""
+
+# Window-related code for the BGE.
+
+from bge import logic, render, events
+
+
+def get_aspect_ratio(width_first=True):
+    """
+    Returns the aspect ratio of the game window, or the window's width / the window's height.
+    If width_first is True (default), then it will return the window's height / the window's width.
+    :param width_first: Bool - Whether to divide the height by the width.
+    :return: Float - The aspect ratio of the window
+    """
+
+    if width_first:
+
+        return render.getWindowWidth() / render.getWindowHeight()
+
+    else:
+
+        return render.getWindowHeight() / render.getWindowWidth()
+
+
+def maintain_asr(ratio: float, width=None):
+    """
+    Sets the window's size to be the width specified and the width divided by the desired aspect ratio as the height.
+    You can use this to ensure you maintain a 16:9 view, or 4:3, or whatever ratio you desire.
+    :param width:Int - Desired width of the window. If left to None, is left to the current window width.
+    :param ratio:Float - Desired aspect ratio to maintain.
+    :return:None
+    """
+
+    if width is None:
+        width = render.getWindowWidth()
+
+    render.setWindowSize(int(width), int(width / ratio))
+
+
+def fullscreen_on_keypress(fs_key=events.F4KEY, *modifier_keys):
+    """
+    Causes full-screen on keypress of fs_key. Also checks to see if modifier keys are being held down.
+    :param fs_key:Int - Keyboard key constant (e.g. events.F4KEY).
+    :param modifier_keys:Int - Other keys that might need to be held down while the fs_key is pushed.
+    :return: Bool or None - The new fullscreen state if triggered (i.e. True or False). Otherwise, None.
+    """
+
+    if logic.keyboard.events[fs_key] == logic.KX_INPUT_JUST_ACTIVATED:
+
+        all_held = True
+
+        for m in modifier_keys:
+
+            if not logic.keyboard.events[m] == logic.KX_INPUT_ACTIVE:
+                all_held = False
+
+        if all_held:
+            new_fs = not render.getFullScreen()
+
+            render.setFullScreen(new_fs)
+
+            return new_fs
+
+    return None
